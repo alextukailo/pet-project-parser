85b7662d70948bb9418870d7bcd953ec08f6be95
alextukailo <alexander.tukaylo@jaydevs.com>
2022-04-11 17:55:34 +0300

    WIP

---
+++ date/month.js
@@ -0,0 +1,66 @@
+const months = () => {
+  return [
+    {
+      id: 1,
+      ru: "января",
+      en: "January",
+    },
+    {
+      id: 2,
+      ru: "февраля",
+      en: "February",
+    },
+    {
+      id: 3,
+      ru: "марта",
+      en: "March",
+    },
+    {
+      id: 4,
+      ru: "апреля",
+      en: "April",
+    },
+    {
+      id: 5,
+      ru: "мая",
+      en: "May",
+    },
+    {
+      id: 6,
+      ru: "июня",
+      en: "June",
+    },
+    {
+      id: 7,
+      ru: "июля",
+      en: "July",
+    },
+    {
+      id: 8,
+      ru: "августа",
+      en: "August",
+    },
+    {
+      id: 9,
+      ru: "сентября",
+      en: "September",
+    },
+    {
+      id: 10,
+      ru: "октября",
+      en: "October",
+    },
+    {
+      id: 11,
+      ru: "ноября",
+      en: "November",
+    },
+    {
+      id: 12,
+      ru: "декабря",
+      en: "December",
+    },
+  ];
+};
+
+module.exports = months;
